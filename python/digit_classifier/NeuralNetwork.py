3001d66947e9140514568e7c82990e3a61e2636a
Anastasia Tarkanii <nastyatrko@gmail.com>
2017-08-13 19:20:07 +0300

    SGD

--- python/digit_classifier/NeuralNetwork.py
+++ python/digit_classifier/NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 def sigmoid(z):
@@ -12,11 +13,59 @@ class NeuralNetwork:
         self.layers_size = layers_size
         self.layers_num = len(self.layers_size)
 
+        np.random.seed(1)
+
         # Initialize weights and bias for each layer with random numbers
         self.weights = [np.random.random(size=(self.layers_size[i + 1], self.layers_size[i]))
                         for i in range(self.layers_num - 1)]
 
         self.biases = [np.random.random(size) for size in self.layers_size[1:]]
+
+    def backpropagation(self, x, y):
+
+        pass
+
+    def update_mini_batch(self, mini_batch, alpha):
+
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+
+        for x, y in mini_batch:
+
+            delta_nabla_b, delta_nabla_w = self.backpropagation(x, y)
+
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+
+        self.weights = [w - (alpha / len(mini_batch)) * nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (alpha / len(mini_batch)) * nb
+                       for b, nb in zip(self.biases, nabla_b)]
+
+    def evaluate(self, test_data):
+
+        pass
+
+    def SGD(self, training_data, epochs, mini_batch_size, alpha=0.01, test_data=None):
+
+        if test_data: n_test = len(test_data)
+
+        for j in range(epochs):
+
+            random.shuffle(training_data)
+
+            mini_batches = [
+                training_data[k:k + mini_batch_size]
+                for k in range(0, len(training_data), mini_batch_size)
+            ]
+
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, alpha)
+
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test))
+            else:
+                print("Epoch {0} complete".format(j))
 
     def predict(self, x):
 
@@ -24,7 +73,7 @@ class NeuralNetwork:
 
         for w, b in zip(self.weights, self.biases):
 
-            a = sigmoid(np.sum(w * a.T, axis=1) + b)
+            a = sigmoid(np.dot(w, a) + b)
 
         return a
 
